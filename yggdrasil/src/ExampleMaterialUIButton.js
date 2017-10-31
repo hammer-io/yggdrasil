025648a0b66dfe1228649a94bcf35b6d6e251917
Nathan De Graaf <ndegraaf@iastate.edu>
2017-10-31 18:22:09 -0500

    Simple back button and some clean up

--- yggdrasil/src/ExampleMaterialUIButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-
-const MyAwesomeReactComponent = () => (
-    <RaisedButton label="Simple Button" primary={true}/>
-);
-
-export default MyAwesomeReactComponent;
