9819451976bd5bd923dfa12ce93e4b5c8f213e30
KrashLeviathan <nathankarasch@gmail.com>
2017-11-06 15:23:38 -0600

    Ignored line breaking linter

--- endor/src/db/sequelize.js
+++ endor/src/db/sequelize.js
@@ -1,5 +1,6 @@
 import Sequelize from 'sequelize';
 
+// eslint-disable-next-line import/no-unresolved
 import dbConfig from '../../../dbConfig.json';
 
 // Create the sequelize instance (once for the app)
