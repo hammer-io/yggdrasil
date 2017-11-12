e108e88f3ca7746b3ed0dcfe953a1beeda126a60
Jack Meyer <jackcmeyer@gmail.com>
2017-11-12 11:35:10 -0600

    moved docs to doc and added project documentation

---
+++ endor/doc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "endor",  "version": "1.0.0",  "description": "API for Yggdrasil",  "title": "endor API",  "url": "localhost:3000/api/v1",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-11-12T17:33:19.375Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
