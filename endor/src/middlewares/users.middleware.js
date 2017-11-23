3298dfc9d9a67d2063a7ea28e7ba746113fa7171
Jack Meyer <jackcmeyer@gmail.com>
2017-11-22 18:40:08 -0600

    removed aysnc validations

--- endor/src/middlewares/users.middleware.js
+++ endor/src/middlewares/users.middleware.js
@@ -3,30 +3,16 @@ import { check } from 'express-validator/check';
 
 let userService = {};
 
-function checkDuplicateUsername(value) {
-  return userService.getUserByIdOrUsername(value).then((user) => {
-    throw new Error(`User with username ${user.email} already exists.`);
-  });
-}
-
-function checkDuplicateEmail(value) {
-  return userService.getUserByEmail(value).then((user) => {
-    throw new Error(`User with email ${user.email} already exists.`);
-  });
-}
-
 /**
  * Middleware for the POST /users route
  * @returns {Array} the middlewares
  */
 export function checkCreateUser() {
   return [
-    check('username').exists().withMessage('Username is required.')
-      .custom(value => checkDuplicateUsername(value)),
+    check('username').exists().withMessage('Username is required.'),
     check('email')
       .exists().withMessage('Email is required.')
       .isEmail().withMessage('Must be a valid email.')
-      .custom(value => checkDuplicateEmail(value))
       .trim()
       .normalizeEmail(),
     check('firstName').exists().withMessage('First Name is required.'),
@@ -40,10 +26,9 @@ export function checkCreateUser() {
  */
 export function checkUpdateUser() {
   return [
-    check('username').custom(value => checkDuplicateUsername(value)),
+    check('username'),
     check('email')
       .isEmail().withMessage('Must be a valid email.')
-      .custom(value => checkDuplicateEmail(value))
       .trim()
       .normalizeEmail(),
   ]
