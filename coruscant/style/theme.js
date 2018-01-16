9225ad05c1bd2437b25d9758af2fdb0781c5f7ce
nischayv <nischay.v@gmail.com>
2018-01-15 20:53:15 -0800

    update theme and add colors

--- coruscant/style/theme.js
+++ coruscant/style/theme.js
@@ -1,49 +1,14 @@
+import * as Colors from './colors'
+
 const theme = {
-  spacing: {
-    tiny: 8,
-    small: 16,
-    regular: 24,
-    large: 48,
-    xLarge: 72
+  padding: {
+    tiny: 16,
+    small: 24,
+    regular: 32,
+    large: 48
   },
 
-    height: {
-        small: 24,
-        regular: 48,
-        large: 72
-    },
-
-    simplePadding: {
-        padding: 20
-    },
-
-  colors: {
-    white: '#FFFFFF',
-    black: '#000000',
-
-    // primary/greys
-    grey200: '#F8F9FB',
-    grey300: '#EEEFF1',
-    grey400: '#E6E7EA',
-    grey500: '#A4AAB4',
-    grey600: '#5A5E6D',
-    grey700: '#343741',
-    grey800: '#20212B',
-    grey900: '#191A1E',
-
-    // secondary colors
-    red: '#E41B41',
-    brown: '#7A5D4B',
-    orange: '#FF794F',
-    amber: '#F3BA41',
-    yellow: '#F0DE3A',
-    lightGreen: '#8BBA55',
-    green: '#21915A',
-    teal: '#43C3B7',
-    blue: '#3B94E3',
-    purple: '#7161B4',
-    transparent: 'rgba(0, 0, 0, 0)'
-  },
+  colors: Colors,
 
   font: {
     family: {
@@ -51,6 +16,7 @@ const theme = {
       bold: 'Roboto-Medium',
       thin: 'Roboto-Light'
     },
+
     small: {
       size: 12,
       lineHeight: 16
